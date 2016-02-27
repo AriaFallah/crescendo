8c657dc6c3fc1f93827620b6e4cb4ea73557d2cd
Walter Berggren <walter.berggren@gmail.com>
2016-02-27 11:24:38 -0800

    Added a class to listen for Myo events

--- myodata.py
+++ myodata.py
@@ -1,4 +1,15 @@
 from time import sleep
-from myo import init, Hub, DeviceListener
+import myo as libmyo
 
-init("myo.framework")
+libmyo.init("myo.framework")
+
+class Listener(libmyo.DeviceListener):
+
+    def on_pair(self, myo, timestamp, firmware_version):
+        print("Hello, Myo!")
+
+    def on_unpair(self, myo, timestamp):
+        print("Goodbye, Myo!")
+
+    def on_orientation_data(self, myo, timestamp, quat):
+        print("Orientation:", quat.x, quat.y, quat.z, quat.w)

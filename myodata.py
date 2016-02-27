c65f699b5db9feee31674b5d23a09e794b6919ca
Walter Berggren <walter.berggren@gmail.com>
2016-02-27 13:45:22 -0800

    Added assignment of motions to midi channels

--- myodata.py
+++ myodata.py
@@ -2,11 +2,15 @@ from time import sleep
 import myo as libmyo
 import rtmidi_python as rtmidi
 
+roll_channel = 3
+pitch_channel = 9
+yaw_channel = 14
+
 class Listener(libmyo.DeviceListener):
 
     midi_out = None
 
-    def __init__(self, midi_out):
+    def __init__(self, midi_out, roll_channel, pitch_channel, yaw_channel):
         self.midi_out = midi_out
 
     def on_pair(self, myo, timestamp, firmware_version):
@@ -20,19 +24,32 @@ class Listener(libmyo.DeviceListener):
         pitch = round(127 * ((quat.pitch + 3.14) / 6.28))
         yaw = round(127 * ((quat.yaw + 3.14) / 6.28))
         print "%d x %d x %d" % (roll, pitch, yaw)
-        self.midi_out.send_message([0xB0, 3, roll])
-        self.midi_out.send_message([0xB0, 9, pitch])
-        self.midi_out.send_message([0xB0, 14, yaw])
+        self.midi_out.send_message([0xB0, roll_channel, roll])
+        self.midi_out.send_message([0xB0, pitch_channel, pitch])
+        self.midi_out.send_message([0xB0, yaw_channel, yaw])
 
 #Initialize the midi controller
 #[0xC0, channel (0-127 as DEC), value (0-127 as DEC)]
 midi_out = rtmidi.MidiOut()
 midi_out.open_virtual_port("myo")
 
+#Assign the channels
+result = raw_input("Assigning roll: Set the application to listen to controller input. Press Enter to continue (or s to skip).\n")
+if result != "s":
+    midi_out.send_message([0xB0, roll_channel, 0])
+
+result = raw_input("Assigning pitch: Set the application to listen to controller input. Press Enter to continue (or s to skip).\n")
+if result != "s":
+    midi_out.send_message([0xB0, pitch_channel, 0])
+
+result = raw_input("Assigning yaw: Set the application to listen to controller input. Press Enter to continue (or s to skip).\n")
+if result != "s":
+    midi_out.send_message([0xB0, yaw_channel, 0])
+
 #Initialize the libmyo controller
 libmyo.init("myo.framework")
 hub = libmyo.Hub()
-myoListener = Listener(midi_out)
+myoListener = Listener(midi_out, roll_channel, pitch_channel, yaw_channel)
 hub.run(1000, myoListener)
 
 try:

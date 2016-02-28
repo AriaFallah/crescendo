4b02d92e605bdfe3aefafd845b25e710e7f94d4b
AriaFallah <aria@fallah.us>
2016-02-28 08:08:53 -0500

    :baby: child process working

--- electron/app/index.js
+++ electron/app/index.js
@@ -1,3 +1,4 @@
+const spawn = require('child_process').spawn
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory, Link } from 'react-router'
@@ -8,11 +9,23 @@ const HomePage = () =>
     <img className="logo" src="logo.png" />
     <h3 className="subheading">Making Music Intuitive</h3>
     <Link to="/configure">
-      <button className="ui button">Get Started</button>
+      <button className="ui button animate">Get Started</button>
     </Link>
   </div>
 
 class ConfigurePage extends Component {
+  componentWillMount() {
+    const p = spawn('python', ['/Users/Developer/workspace/hackathons/hacktech/sensor/sensor2midi.py'])
+    p.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`)
+    })
+    p.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`)
+    })
+    p.on('close', (code) => {
+      console.log(`child process exited with code ${code}`)
+    })
+  }
   render() {
     return (
       <div>

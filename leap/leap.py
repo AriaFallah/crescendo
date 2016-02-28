965655b58769e7ae0ce3fa2376437be19d6173bb
rargh <eetu.t.narhi@gmail.com>
2016-02-28 06:39:06 +0200

    Added midi functionality

--- leap/leap.py
+++ leap/leap.py
@@ -6,21 +6,20 @@ from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
 class SampleListener(Leap.Listener):
 
+    midi_out = None
 
+    def __init__(self):
+        self.midi_out = rtmidi.MidiOut()
+        self.midi_out.open_virtual_port("Leap")
+        super(SampleListener, self).__init__()   
 
-    def on_connect(self, controller):
-        print "Connected"
- 
     def on_frame(self, controller):
         frame = controller.frame()
         hand = frame.hands.rightmost
         hand_cent = hand.palm_position
-        handy = hand_cent[1]
-        handx = hand_cent[0]
-        handz = hand_cent[2]
-        mody = round((((handy - 45) / 400) * 127), 0)
-        modx = round((((handx + 150) / 300) * 127), 0)
-        modz = round((((handz + 150) / 300) * 127), 0)
+        mody = round((((hand_cent[1] - 45) / 400) * 127), 0)
+        modx = round((((hand_cent[0] + 150) / 300) * 127), 0)
+        modz = round((((hand_cent[2] + 150) / 300) * 127), 0)
         if mody > 127:
             mody = 127
         if mody < 0:
@@ -33,17 +32,20 @@ class SampleListener(Leap.Listener):
             modz = 127
         if modz < 0:
             modz = 0
-        if hand.time_visible == 0:
-            print "Hand not visible"
-        else:
+        if hand.time_visible != 0:
+            self.midi_out.send_message([0xB0, 3, mody])
+            self.midi_out.send_message([0xB0, 9, modx])
+            self.midi_out.send_message([0xB0, 14, modz])
             print mody, modx, modz
 
+
 def main():
+    midi_out = rtmidi.MidiOut()
+    midi_out.open_virtual_port("Leap")
     listener = SampleListener()
     controller = Leap.Controller()
     controller.add_listener(listener)
     # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
     try:
         sys.stdin.readline()
     except KeyboardInterrupt:

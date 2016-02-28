c0f62177140e6c899e1b30726d62216f18ceb80f
AriaFallah <aria@fallah.us>
2016-02-28 12:48:16 -0500

    :art: I AM AN ARTIST

--- leap/leap.js
+++ leap/leap.js
@@ -38,7 +38,7 @@ co(function* () {
       const position = hand.palmPosition
 
       let modx = Math.round((((position[0] + 150) / 300) * 127), 0)
-      let mody = Math.round((((position[1] - 45) / 400) * 127),  0)
+      let mody = Math.round((((position[1] -  45) / 400) * 127), 0)
       let modz = Math.round((((position[2] + 150) / 300) * 127), 0)
 
       if (modx > 127) modx = 127
@@ -49,9 +49,9 @@ co(function* () {
       if (modz <   0) modz = 0
 
       if (hand.timeVisible !== 0) {
-        output.sendMessage([0xB0, 3,  mody])
-        output.sendMessage([0xB0, 9,  modx])
-        output.sendMessage([0xB0, 14, modz])
+        output.sendMessage([0xB0,  3,  mody])
+        output.sendMessage([0xB0,  9,  modx])
+        output.sendMessage([0xB0, 14,  modz])
         console.log(modx, mody, modz)
       }
     }
